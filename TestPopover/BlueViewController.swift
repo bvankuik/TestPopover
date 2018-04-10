61157e56c08116dd09cff8fc06d9da9c48d1e1e4
Bart van Kuik <bart@vankuik.nl>
2018-04-10 08:15:37 +0200

    First version with UIBarButtonItem and UIButton

---
+++ TestPopover/BlueViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BlueViewController.swift
+//  TestPopover
+//
+//  Created by Bart van Kuik on 10/04/2018.
+//  Copyright © 2018 DutchVirtual. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.preferredContentSize = CGSize(width: 1024, height: 1024)
+    }
+}
---
+++ TestPopover/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  TestPopover
+//
+//  Created by Bart van Kuik on 10/04/2018.
+//  Copyright © 2018 DutchVirtual. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        self.view.backgroundColor = .lightGray
+    }
+}
